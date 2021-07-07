d87312829bc61eeefaaef6ecf2f547bc0a324d80
Rachel Jensen <rachelangela@gmail.com>
2021-07-07 16:17:50 -0600

    Add Round class

--- test/Round-test.js
+++ test/Round-test.js
@@ -35,67 +35,69 @@ describe('Round', function() {
     expect(round.numberOfCards).to.equal(3);
   });
 
-  it.skip('should be able to draw a card', function() {
-    //returnCurrentCard is a method that returns the card being played
+  it('should be able to draw a card', function() {
     round.returnCurrentCard();
 
     expect(round.currentCard).to.equal(card1)
   });
 
-  it.skip('should update count the number of turns', function() {
+  it('should count the number of turns', function() {
     expect(round.turns).to.equal(0);
 
     round.takeTurn('Dino Floof');
 
     expect(round.turns).to.equal(1);
-
   });
 
-  it.skip('should evalute if guess is incorrect', function() {
+  it('should count the number of turns regardless of correct answer', function() {
+    expect(round.turns).to.equal(0);
+
+    round.takeTurn('Lady');
+
+    expect(round.turns).to.equal(1);
+  });
+
+  it('should evalute if guess is incorrect', function() {
     let answer = round.takeTurn('Flooficorn');
 
     expect(answer).to.equal('incorrect!');
   });
 
-  it.skip('should evalute if guess is correct', function() {
+  it('should evalute if guess is correct', function() {
     let answer = round.takeTurn('Lady');
 
     expect(answer).to.equal('correct!');
   });
 
-  it.skip('should store incorrect guesses', function() {
-    //takeTurn()
+  it('should store incorrect guesses', function() {
     round.takeTurn('Dino Floof');
-    //incorrectGuesses array
+
     expect(round.incorrectAnswers).to.be.a('array');
-    //collect the ids of incorrectly guessed cards
     expect(round.incorrectAnswers[0]).to.equal(0);
   });
 
-  it.skip('should update the currentCard to the next card in deck', function() {
-    //takeTurn();
+  it('should update the currentCard to the next card in deck', function() {
     round.takeTurn('Lady');
 
     expect(round.currentCard).to.equal(card2)
   });
 
-  it.skip('should calculate the percentage of correct guesses', function() {
-
+  it('should calculate the percentage of correct guesses', function() {
     round.takeTurn('Lady'); //should be correct
     round.takeTurn('Topo'); //should be incorrect
-    //calculatePercentageCorrect()
+
     let percentRight = round.calculatePercentageCorrect();
 
     expect(percentRight).to.equal(50);
   });
 
-  it.skip('should end the round when all the cards have been played', function() {
+  it('should end the round when all the cards have been played', function() {
     round.takeTurn('Lady');
     round.takeTurn('Topo');
     round.takeTurn('The Chariot');
-    //endRound();
+
     let message = round.endRound();
-    //message "**Round over!** You answered x% of the questions correctly!"
+
     expect(message).to.equal('**Round over!** You answered 67% of the questions correctly!')
   });
 });
