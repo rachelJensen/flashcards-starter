458de3dcc1433cb3b9dccf90db6609264e0c26f9
Rachel Jensen <rachelangela@gmail.com>
2021-07-08 16:35:09 -0600

    Refactor with beforeEach

--- test/Game-test.js
+++ test/Game-test.js
@@ -21,33 +21,30 @@ const prototypeQuestions = [{
   }];
 
 describe('The Game', function() {
+  let game;
+
+  beforeEach(function() {
+    game = new Game;
+    game.start();
+  });
 
   it('should be a function', function() {
-
     expect(Game).to.be.a('function');
   });
 
   it('should be an instantiation of Game', function() {
-    let game = new Game();
-
     expect(game).to.be.an.instanceof(Game)
   });
 
-  it.skip('should be able to create Cards', function() {
-    let newGame = game.start(rawData);
-
+  it('should be able to create Cards', function() {
     expect(game.cards[0]).to.be.an.instanceof(Card);
   });
 
-  it.skip('should be able to create a Deck with multiple Cards', function() {
-    let newGame = game.start();
-
+  it('should be able to create a Deck with multiple Cards', function() {
     expect(game.deck).to.be.an.instanceof(Deck);
   });
 
-  it.skip('should be able to start a new Round', function() {
-    let newGame = game.start();
-
+  it('should be able to start a new Round', function() {
     expect(game.currentRound).be.an.instanceof(Round);
   });
 });
